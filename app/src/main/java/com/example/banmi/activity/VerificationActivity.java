e364ec8bd89059142dd172d9b980262bd951d938
qizhiyangguang <2206411563@qq.com>
2019-05-23 21:18:47 +0800

    盼盼

---
+++ app/src/main/java/com/example/banmi/activity/VerificationActivity.java
@@ -0,0 +1,15 @@
+package com.example.banmi.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.banmi.R;
+
+public class VerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+    }
+}
