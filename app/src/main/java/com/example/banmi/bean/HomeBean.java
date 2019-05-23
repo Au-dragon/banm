6d6e96409da0d8e6837904014530680908701cfc
zhangfan <1015053269@qq.com>
2019-05-23 20:29:16 +0800

    张凡上传的

---
+++ app/src/main/java/com/example/banmi/bean/HomeBean.java
@@ -0,0 +1,4 @@
+package com.example.banmi.bean;
+
+public class HomeBean {
+}
---
+++ app/src/main/java/com/example/banmi/view/home/HomeView.java
@@ -0,0 +1,6 @@
+package com.example.banmi.view.home;
+
+import com.example.banmi.base.BaseView;
+
+public interface HomeView extends BaseView {
+}
